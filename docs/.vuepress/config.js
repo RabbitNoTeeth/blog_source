a95df881155efb7fc809d3d56259d07a932ede19
liuxindong <864426487@qq.com>
2024-02-20 17:53:41 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    themeConfig: {
+        logo: 'asdasd',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    }
+}
